67b7ae16f0561a7af01f3f55ba5100437e672446
rishabhkumar17 <rishabhkaushal1796@gmail.com>
2023-07-26 15:41:47 +0530

    functional composition

--- src/index.js
+++ src/index.js
@@ -8,3 +8,14 @@ function sayHello() {
 
 let fn = sayHello();
 let message = fn();
+
+// Functional Composition
+
+let input = "   JavaScript  ";
+let output = "<div>" + input.trim() + "</div>";
+
+const trim = str => str.trim();
+const wrapInDiv = str => `<div>${str}</div>`;
+const toLowerCase = str => str.toLowerCase();
+
+const result = wrapInDiv(toLowerCase(trim(input)));
